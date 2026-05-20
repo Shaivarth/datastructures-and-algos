834d1845b377078ee0add3c7f9cd0fd966de2974
Shaivarth <sarthakmishra080803@gmail.com>
2026-05-20 14:23:00 +0530

    valid palindrome

--- Arrays/easy/valid_palindrome.cpp
+++ Arrays/easy/valid_palindrome.cpp
@@ -5,43 +5,31 @@ using namespace std;
 class Solution {
 public:
     bool isPalindrome(string s) {
-
         int left = 0;
         int right = s.length() - 1;
-
         while (left < right) {
-
-            // Skip non-alphanumeric characters
+            // Skiping non-alphanumeric characters 
             while (left < right && !isalnum(s[left])) {
                 left++;
             }
-
             while (left < right && !isalnum(s[right])) {
                 right--;
             }
-
-            // Convert uppercase to lowercase and compare
+            // Converting uppercase to lowercase and comparing
             if (tolower(s[left]) != tolower(s[right])) {
                 return false;
             }
-
             left++;
             right--;
         }
-
         return true;
     }
 };
 
 int main() {
-
     string s = "A man, a plan, a canal: Panama";
-
     Solution obj;
-
     bool result = obj.isPalindrome(s);
-
     cout << boolalpha << result;
-
     return 0;
 }
